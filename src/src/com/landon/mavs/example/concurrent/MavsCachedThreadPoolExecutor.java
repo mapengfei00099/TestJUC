d64fbd3a8e7f6ad173dba679c47f32b3f3331e6e
马鹏飞 <mapengfei00099@163.com>
2015-07-12 11:49:18 +0800

    first commit

---
+++ src/src/com/landon/mavs/example/concurrent/MavsCachedThreadPoolExecutor.java
@@ -0,0 +1,29 @@
+package com.landon.mavs.example.concurrent;
+
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 
+ * Mavs Cacheœﬂ≥Ã≥ÿ {@link java.util.concurrent.Executors#newCachedThreadPool()}
+ * 
+ * @author landon
+ * 
+ */
+public class MavsCachedThreadPoolExecutor extends MavsThreadPoolExecutor {
+
+	public MavsCachedThreadPoolExecutor() {
+		super(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+	}
+
+	public MavsCachedThreadPoolExecutor(ThreadFactory threadFactory) {
+		super(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), threadFactory);
+	}
+
+	public MavsCachedThreadPoolExecutor(ThreadFactory threadFactory, RejectedExecutionHandler rejectHandler) {
+		super(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), threadFactory,
+				rejectHandler);
+	}
+}
